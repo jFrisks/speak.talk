a6b0c70333848ae3e27d090c45c2452799700d83
jfrisks <jo1875fr-s@student.lu.se>
2020-09-22 14:00:21 +0200

    Implemented fully globa var for MSN

--- labbar/lab3/A3-SimpliC/src/java/lang/MSNVisitor.java
+++ labbar/lab3/A3-SimpliC/src/java/lang/MSNVisitor.java
@@ -2,6 +2,32 @@ package lang;
 
 import lang.ast.*;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class MSNVisitor extends TraversingVisitor {
+    private static int maxDepth = 0;
+    private static int depth = 0;
+
+    public static int result(ASTNode n) {
+        MSNVisitor v = new MSNVisitor();
+        n.accept(v, 0);
+        return v.maxDepth;
+    }
+
+    public Object visit(Block node, Object data) {
+        depth += 1;
+        if(depth > maxDepth){
+            maxDepth = depth;
+        }
+        visitChildren(node, null);
+        depth -= 1;
+        return data;
+    }
+}
+
+/*
 public class MSNVisitor extends TraversingVisitor {
     private static int maxDepth = 0;
     public static int result(ASTNode n) {
@@ -19,3 +45,4 @@ public class MSNVisitor extends TraversingVisitor {
         return data;
     }
 }
+*/
