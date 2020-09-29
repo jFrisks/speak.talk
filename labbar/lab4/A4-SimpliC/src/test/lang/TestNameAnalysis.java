ceeddda4d23f264232d4a7a564b6051328adad52
LucasEdlund <lucas.edlund@gmail.com>
2020-09-29 12:06:11 +0200

    Added errors, unfinished

--- labbar/lab4/A4-SimpliC/src/test/lang/TestNameAnalysis.java
+++ labbar/lab4/A4-SimpliC/src/test/lang/TestNameAnalysis.java
@@ -1,26 +1,22 @@
 package lang;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.PrintStream;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+import lang.ast.ErrorMessage;
 import lang.ast.Program;
 
 /**
  * This is a parameterized test: one test case is generated for each input
  * file found in TEST_DIRECTORY. Input files should have the ".in" extension.
  */
-/*
 @RunWith(Parameterized.class)
 public class TestNameAnalysis {
-    */
-/** Directory where the test input files are stored. *//*
-
+    /** Directory where the test input files are stored. */
     private static final File TEST_DIRECTORY = new File("testfiles/nameanalysis");
 
     private final String filename;
@@ -30,9 +26,11 @@ public class TestNameAnalysis {
 
     @Test public void runTest() throws Exception {
         Program program = (Program) Util.parse(new File(TEST_DIRECTORY, filename));
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        program.checkNames(new PrintStream(bytes));
-        String actual = bytes.toString();
+        StringBuilder sb = new StringBuilder();
+        for (ErrorMessage m : program.errors()) {
+            sb.append(m).append("\n");
+        }
+        String actual = sb.toString();
         Util.compareOutput(actual,
                 new File(TEST_DIRECTORY, Util.changeExtension(filename, ".out")),
                 new File(TEST_DIRECTORY, Util.changeExtension(filename, ".expected")));
@@ -43,4 +41,3 @@ public class TestNameAnalysis {
         return Util.getTestParameters(TEST_DIRECTORY, ".in");
     }
 }
-*/
