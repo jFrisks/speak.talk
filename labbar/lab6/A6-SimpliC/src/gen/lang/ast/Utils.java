825ab436f453be176b19fc16c3b10579d28fecfb
Alice <lucas.edlund@gmail.com>
2020-10-15 10:53:50 +0200

    GenCode - almost done with if/while

--- labbar/lab6/A6-SimpliC/src/gen/lang/ast/Utils.java
+++ labbar/lab6/A6-SimpliC/src/gen/lang/ast/Utils.java
@@ -17,6 +17,29 @@ public class Utils extends java.lang.Object {
             return b ? 1 : 0;
         }
 
+  
+
+        public static String uniqueNameRec(ASTNode node, String currentString) {
+            if(node instanceof FunctionDecl){
+                //System.out.println("Var name is: " + currentString);
+                return currentString;
+            }
+            else if(node instanceof Stmt){
+                //get index of stmt
+                int index = node.getParent().getIndexOfChild(node);
+                //System.out.println(node.getParent().getClass().getName() + " has " + node.getClass().getName() + " on index " + index);
+                //add it to currentString
+                String newString = index + "_" + currentString;
+                //System.out.println("Unfinished unique is: "+newString);
+
+                //recursively call
+                return uniqueNameRec(node.getParent(), newString);
+            }
+            else{
+                return uniqueNameRec(node.getParent(), currentString);
+            }
+        }
+
 
 
 }
